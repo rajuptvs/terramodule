4b2bb55305fa69a165d09955a4e2471d04e72089
rajuptvs <raju.ptvs@gmail.com>
2023-07-04 11:13:32 -0400

    added gitignore

---
+++ module-example/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  # (parameters hidden for clarity)
+
+  cluster_name           = var.cluster_name
+  db_remote_state_bucket = "tf23"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size      = 1
+  max_size      = 2
+}
+
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type              = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
+
+  from_port   = 12345
+  to_port     = 12345
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
